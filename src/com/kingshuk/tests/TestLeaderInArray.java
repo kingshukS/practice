146d369ff30076ab4e6dec03bed8ea1a8a471f53
Kingshuk Saha <kingshuk1808@gmail.com>
2021-06-06 00:24:01 +0530

    Convert an Array to Wave Array, Leaders in Array in Order, Find the maximum of j - i subjected to the constraint of A[i] <= A[j]

--- src/com/kingshuk/tests/TestLeaderInArray.java
+++ src/com/kingshuk/tests/TestLeaderInArray.java
@@ -4,17 +4,19 @@ public class TestLeaderInArray {
     public static void main(String[] args) {
         int[] arr = {8,6,3,4,5};
         int n = arr.length;
-        printLeader(arr,n);
+        printLeaderInOrder(arr,n);
     }
 
-    private static void printLeader(int[] arr, int n) {
+    private static void printLeaderInOrder(int[] arr, int n) {
         int leader = arr[n-1];
-        System.out.print(leader+" ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(leader+" ");
         for(int i = n-2; i>=0; i--){
             if(arr[i]>leader){
                 leader = arr[i];
-                System.out.print(leader+" ");
+                sb.append(leader+" ");
             }
         }
+        System.out.println(sb.reverse().toString());
     }
 }
