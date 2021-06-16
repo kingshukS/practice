9353be20ebd1a159e4531aa2d35556d81bf9fa4c
Kingshuk Saha <kingshuk1808@gmail.com>
2021-06-16 10:26:04 +0530

    Square Root using binary search, Unbounded binary search

---
+++ src/com/kingshuk/tests/TestSquareRootBinarySearch.java
@@ -0,0 +1,29 @@
+package com.kingshuk.tests;
+
+public class TestSquareRootBinarySearch {
+    public static void main(String[] args) {
+        int x = 17;
+        int ans = findSquareRoot(x);
+        Math.sqrt(10);
+        System.out.println("Square Root of x="+x+" is = "+ans);
+    }
+
+    private static int findSquareRoot(int x) {
+        int low = 1;
+        int high = x;
+        int ans = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int msq = mid * mid;
+            if(msq == x)
+                return mid;
+            else if(msq>x)
+                high = mid -1;
+            else{
+                ans = mid;
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
