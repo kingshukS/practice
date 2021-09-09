a0a654cd27ee24103e9272634986e6d1687d9dd6
Kingshuk Saha <kingshuk1808@gmail.com>
2021-09-09 22:37:45 +0530

    Bubble Sort Optimized
    In a pass, if no swapping happens, then the array
    is already sorted.

---
+++ src/com/kingshuk/tests/sorting/BubbleSort.java
@@ -0,0 +1,33 @@
+package com.kingshuk.tests.sorting;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,7,8,3,0,1,7,2};
+        applyBubbleSort(arr);
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void applyBubbleSort(int[] arr) {
+        boolean swapped = false;
+        int n = arr.length;
+        if(n<2)
+            return;
+        for(int j = 0; j<n-1; j++){
+            swapped = false;
+            for(int i = 0; i<n-j-1; i++){
+                if(arr[i] > arr[i+1])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+                break;
+        }
+    }
+}
