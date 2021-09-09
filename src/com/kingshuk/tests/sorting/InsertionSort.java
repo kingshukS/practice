389c6e5d4671d3817c0c4a211055b67a7cb8e372
Kingshuk Saha <kingshuk1808@gmail.com>
2021-09-09 23:46:50 +0530

    Insertion Sort

---
+++ src/com/kingshuk/tests/sorting/InsertionSort.java
@@ -0,0 +1,29 @@
+package com.kingshuk.tests.sorting;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,7,8,3,0,1,7,2};
+        applyInsertionSort(arr);
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void applyInsertionSort(int[] arr) {
+        int n = arr.length;
+        if(n<2)
+            return;
+        for(int i =1;i<n;i++)
+        {
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && key<arr[j])
+            {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+}
