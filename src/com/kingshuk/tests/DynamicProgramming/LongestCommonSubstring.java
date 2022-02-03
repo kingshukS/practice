3e2f2fc1ea776b0748c2a27893ec8cb0d3d20e93
Kingshuk Saha <kingshuk1808@gmail.com>
2022-02-03 11:05:22 +0530

    Modify LongestCommonSubstring, add method to accept string as input and use one set of loops instead of two

--- src/com/kingshuk/tests/DynamicProgramming/LongestCommonSubstring.java
+++ src/com/kingshuk/tests/DynamicProgramming/LongestCommonSubstring.java
@@ -2,36 +2,31 @@ package com.kingshuk.tests.DynamicProgramming;
 
 public class LongestCommonSubstring {
 
-    static int longestSubSeqlength(char[] x,char[] y,int n ,int m)
-    {
-        int t[][] = new int[n+1][m+1];
+    static int longestCommonSubstringLength(String x, String y, int m, int n) {
+        int t[][] = new int[m + 1][n + 1];
         int result = 0;
-        for(int i = 1;i<=n;i++)
-        {
-            for(int j = 1;j<=m;j++)
-            {
-                if(i == 0 || j==0) //this means if one the lengths of strings is 0 then result will be 0 Base Condition
-                    t[i][j] =  0;
-
-                if(x[i-1] == y[j-1]){
-                    t[i][j] =   1 + t[i-1][j-1];
-                    result = Math.max(result,t[i][j]);}
-                else
-                  t[i][j] = 0;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) //this means if one the lengths of strings is 0 then result will be 0 Base Condition
+                {
+                    t[i][j] = 0;
+                } else if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                    result = Math.max(result, t[i][j]);
+                } else {
+                    t[i][j] = 0;
+                }
             }
         }
+
         return result;
     }
 
-    public static void main(String args[])
-    {
-        String s1 = "ABCDGH";
-        String s2 = "ABDFHR";
-
-        char[] x = s1.toCharArray();
-        char[] y = s2.toCharArray();
-        int n = x.length;
-        int m = y.length;
-        System.out.println("the longest length of substring is : "+longestSubSeqlength(x,y,n,m));
+    public static void main(String args[]) {
+        String s1 = "ABCDGHIJK";
+        String s2 = "ABDFHRIJK";
+        int m = s1.length();
+        int n = s2.length();
+        System.out.println("the longest length of substring is : " + longestCommonSubstringLength(s1, s2, m, n));
     }
 }
