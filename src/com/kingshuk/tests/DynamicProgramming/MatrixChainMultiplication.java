1785a5f01550814ce21f8987f252384334a7386c
Kingshuk Saha <kingshuk1808@gmail.com>
2023-10-06 22:44:44 +0530

    Matrix Chain Multiplication (MCM)

--- src/com/kingshuk/tests/DynamicProgramming/MatrixChainMultiplication.java
+++ src/com/kingshuk/tests/DynamicProgramming/MatrixChainMultiplication.java
@@ -1,13 +1,59 @@
 package com.kingshuk.tests.DynamicProgramming;
 
+import java.util.Arrays;
+
 public class MatrixChainMultiplication {
+    static int count = 0;
+    static int[][] t;
     public static void main(String[] args) {
-        int arr[] = new int[]{1, 2, 3, 4};
+        int arr[] = new int[]{1, 2, 3, 4, 5, 6,7,8};
         int n = arr.length;
         System.out.println("Minimum number of multiplications is "
-                + matrixChainOrder(arr, n));
+                + matrixChainOrder(arr));
+        System.out.println(count);
     }
 
+    private static int matrixChainOrder(int[] arr) {
+        int n = arr.length;
+        int i = 1;
+        int j = n-1;
+        t = new int[n+1][n+1];
+        for(int k = 0; k<=n; k++)
+            Arrays.fill(t[k], -1);
+        return mcmRecursiveMemoization(arr, i, j);
+    }
+
+    private static int mcmRecursive(int[] arr, int i , int j)
+    {
+        count++;
+        if(i>=j)
+            return 0;
+        int min = Integer.MAX_VALUE;
+        for(int k = i; k<j; k++)
+        {
+            min = Math.min(mcmRecursive(arr, i, k)+ mcmRecursive(arr, k+1, j)+ arr[i-1]*arr[k]*arr[j], min);
+        }
+        return min;
+    }
+
+    private static int mcmRecursiveMemoization(int[] arr, int i , int j)
+    {
+        count++;
+        if(i>=j)
+            return 0;
+        if(t[i][j] != -1)
+            return t[i][j];
+        int min = Integer.MAX_VALUE;
+        for(int k = i; k<j; k++)
+        {
+            int x = t[i][k] != -1?t[i][k]:mcmRecursiveMemoization(arr, i, k);
+            //int x = mcmRecursiveMemoization(arr, i, k);
+            int y = t[k+1][j] != -1? t[k+1][j]: mcmRecursiveMemoization(arr, k+1, j);
+            //int y = mcmRecursiveMemoization(arr, k+1, j);
+            min = Math.min(x+y+ arr[i-1]*arr[k]*arr[j], min);
+        }
+        return t[i][j]=min;
+    }
     private static int matrixChainOrder(int[] arr, int n) {
         int[][] t = new int[n][n];
         for (int i = 1; i < n; i++)
