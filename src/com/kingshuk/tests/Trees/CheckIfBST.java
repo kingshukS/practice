5652ebc80c1840f8e4cac92d85f8ad6707b790c1
Kingshuk Saha <kingshk1808@gmail.com>
2024-02-20 22:09:13 +0530

    BST LCA

--- src/com/kingshuk/tests/Trees/CheckIfBST.java
+++ src/com/kingshuk/tests/Trees/CheckIfBST.java
@@ -7,38 +7,45 @@ import java.util.Stack;
 
 public class CheckIfBST {
 
-    static boolean checkForBST(NodeT root)
-    {
-        if(root == null)
-        {
+    static boolean checkForBST(NodeT root) {
+        if (root == null) {
             return false;
         }
 
         Stack<NodeT> st = new Stack<>();
         NodeT curr = root;
         ArrayList<Integer> al = new ArrayList<>();
-        while(curr!=null || st.size()>0)
-        {
-            while(curr!=null)
-            {
+        while (curr != null || st.size() > 0) {
+            while (curr != null) {
                 st.push(curr);
                 curr = curr.left;
             }
             curr = st.pop();
             al.add(curr.data);
-            System.out.print(curr.data+" ");
+            System.out.print(curr.data + " ");
             curr = curr.right;
         }
 
-        for(int i= 1;i<=al.size()-1;i++)
-        {
-            if(al.get(i-1)>al.get(i))
+        for (int i = 1; i <= al.size() - 1; i++) {
+            if (al.get(i - 1) > al.get(i))
                 return false;
         }
-       return true;
+        return true;
     }
-    public static void main(String[] args)
-    {
+
+    public boolean isValidBST(TreeNode root) {
+        return isBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isBST(TreeNode root, long left, long right) {
+        if (root == null) return true;
+        if (root.data <= left || root.data >= right)
+            return false;
+
+        return isBST(root.left, left, root.data) && isBST(root.right, root.data, right);
+    }
+
+    public static void main(String[] args) {
         NodeT root = new NodeT(15);
         root.left = new NodeT(13);
         root.right = new NodeT(18);
@@ -47,6 +54,6 @@ public class CheckIfBST {
         root.right.left = new NodeT(16);
         root.right.right = new NodeT(19);
 
-        System.out.println("the given tree is BST : "+checkForBST(root));
+        System.out.println("the given tree is BST : " + checkForBST(root));
     }
 }
