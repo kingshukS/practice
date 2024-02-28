57f391f1ef8883415c321e6e7034426a6af00749
Kingshuk Saha <kingshk1808@gmail.com>
2024-02-28 20:56:43 +0530

    BST from Preorder traversal

---
+++ src/com/kingshuk/tests/Trees/BSTFromPreorder.java
@@ -0,0 +1,17 @@
+package com.kingshuk.tests.Trees;
+
+class BSTFromPreorder {
+
+    private int index = 0;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return bstFromPreorder(preorder, Integer.MAX_VALUE);
+    }
+
+    private TreeNode bstFromPreorder(int[] preorder, int bound) {
+        if(index>=preorder.length || preorder[index] > bound) return null;
+        TreeNode root = new TreeNode(preorder[index++]);
+        root.left = bstFromPreorder(preorder, root.data);
+        root.right = bstFromPreorder(preorder, bound);
+        return root;
+    }
+}
