948e3af8a6e41072ca95d359c59c0d8e1cf92926
Kingshuk Saha <kingshuk1808@gmail.com>
2024-05-20 18:41:35 +0530

    Max Frequency

--- src/com/kingshuk/tests/striver/arrays/MaxFrequency.java
+++ src/com/kingshuk/tests/striver/arrays/MaxFrequency.java
@@ -2,6 +2,11 @@ package com.kingshuk.tests.striver.arrays;
 
 import java.util.Arrays;
 
+/**
+ * 1838. Frequency of the Most Frequent Element
+ * <a href="https://leetcode.com/problems/frequency-of-the-most-frequent-element/description/">
+ */
+
 public class MaxFrequency {
     public static void main(String[] args) {
         int[] arr = {1, 4, 3, 3};
@@ -12,23 +17,32 @@ public class MaxFrequency {
     public static int maxFrequency2(int[] nums, int k) {
         int min = Integer.MAX_VALUE;
         int max = 0;
-        for(int num : nums){
-            min = Math.min(min,num);
-            max = Math.max(max,num);
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
-        int len = max-min+1;
+        int len = max - min + 1;
         int[] count = new int[len];
 
-        for(int num : nums){
-            count[num-min]++;
+        for (int num : nums) {
+            count[num - min]++;
         }
-        int start=0, sum=0,ans=0,cnt=0,startCnt=count[0];
-        for(int i=0;i<len;++i){
-            sum+=cnt;
-            cnt+=count[i];
-            while(sum>k){
-                while(startCnt==0) startCnt = count[++start];
-                sum-=i-start;
+
+        /**
+         * sum = all the inc() operations required in the current window
+         * cnt = number of elements in the current window
+         * start = starting index of the current window
+         * startCnt = starting element frequency
+         * ans = for storing the ans across the sliding process
+         * sum-=i-start decreases the number of inc() operations required after shrinking the window from left
+         */
+        int start = 0, sum = 0, ans = 0, cnt = 0, startCnt = count[0];
+        for (int i = 0; i < len; ++i) {
+            sum += cnt;
+            cnt += count[i];
+            while (sum > k) {
+                while (startCnt == 0) startCnt = count[++start];
+                sum -= i - start;
                 startCnt--;
                 cnt--;
             }
